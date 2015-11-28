c81ec4536b1ec3dae0e61be0beaa44a333c80a64
Hypostasis <michal.andrzej.wrobel@hotmail.com>
2015-11-29 00:16:23 +0100

    Implemented TCP Client & sending JSON packets

--- app/src/main/java/com/example/hypo/neurobotballcontroller/AnimatedView.java
+++ app/src/main/java/com/example/hypo/neurobotballcontroller/AnimatedView.java
@@ -10,9 +10,12 @@ import android.os.Handler;
 
 import android.util.AttributeSet;
 
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 /**
  * Created by hypo on 02.11.15.
@@ -54,19 +57,63 @@ public class AnimatedView extends ImageView {
         this.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(final View v, final MotionEvent event) {
-                //JSONObject packet = new JSONObject();
+                /*
+                    JSON:
+                    {'left_motor': int; 'right_motor': int;}
+                    left_motor - predkość lewego silnika, od 255 do -255; 0 stop
+                    right_motor - predkość prawego silnika, od 255 do -255; 0 stop
+                */
+                JSONObject packet = new JSONObject();
+                int left_motor = 0, right_motor = 0;
+
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.d("  main ", " value of x,y  down" + event.getX() + " " + event.getY());
                     x = (int)event.getX();
                     y = (int)event.getY();
+                    left_motor = 127;
+                    right_motor = 127;
+                    try {
+                        packet.put("left_motor", left_motor);
+                        packet.put("right_motor", right_motor);
+                        Client client = new Client(packet);
+                        client.run();
+                    } catch (JSONException e) {
+                    }
+
                     //packet.put <- wedlug protokolu rafala kozika
                     //run Client Thread new Client(packet).start();
+
                     return true;
                 }
                 else if(event.getAction() == MotionEvent.ACTION_MOVE) {
+                    Log.d("  main ", " value of x,y  down" + event.getX() + " " + event.getY());
                     x = (int)event.getX();
                     y = (int)event.getY();
                     //packet.put <- wedlug protokolu rafala kozika
                     //run Client Thread
+                    left_motor = 127;
+                    right_motor = 127;
+
+                    try {
+                        packet.put("left_motor", left_motor);
+                        packet.put("right_motor", right_motor);
+                        Client client = new Client(packet);
+                        client.run();
+                    } catch (JSONException e) {
+                    }
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    Log.d("  main ", "STOPPED");
+                    left_motor = 0;
+                    right_motor = 0;
+                    try {
+                        packet.put("left_motor", left_motor);
+                        packet.put("right_motor", right_motor);
+                        Client client = new Client(packet);
+                        client.run();
+                    } catch (JSONException e) {
+                    }
+
                     return true;
                 }
                 return false;
---
+++ app/src/main/java/com/example/hypo/neurobotballcontroller/Client.java
@@ -0,0 +1,107 @@
+package com.example.hypo.neurobotballcontroller;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+/**
+ * Created by hypo on 25.11.15.
+ */
+
+//klient tcp/ip, wysyła dane
+public class Client implements Runnable {
+    JSONObject packet;
+    public static final String SERVER_IP = "192.168.0.127"; //your computer IP address
+    public static final int SERVER_PORT = 5678;
+    // message to send to the server
+    private String mServerMessage;
+    // sends message received notifications
+    //private OnMessageReceived mMessageListener = null;
+    // while this is true, the server will continue running
+    private boolean mRun = false;
+    // used to send messages
+    private PrintWriter mBufferOut;
+    // used to read messages from the server
+    private BufferedReader mBufferIn;
+
+    public void sendMessage(String message) {
+        if (mBufferOut != null && !mBufferOut.checkError()) {
+            mBufferOut.println(message);
+            mBufferOut.flush();
+        }
+    }
+
+    public void stopClient() {
+        Log.i("Debug", "stopClient");
+
+        // send mesage that we are closing the connection
+        //sendMessage(Constants.CLOSED_CONNECTION + "Kazy");
+
+        mRun = false;
+
+        if (mBufferOut != null) {
+            mBufferOut.flush();
+            mBufferOut.close();
+        }
+
+        //mMessageListener = null;
+        mBufferIn = null;
+        mBufferOut = null;
+        mServerMessage = null;
+    }
+
+    public Client(JSONObject packet) {
+        this.packet = packet;
+    }
+
+    @Override
+    public void run() {
+        //bind port
+        //send (packet.toString())
+        mRun = true;
+
+        try {
+            //here you must put your computer's IP address.
+            InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+
+            Log.e("TCP Client", "C: Connecting...");
+
+            //create a socket to make the connection with the server
+            Socket socket = new Socket(serverAddr, SERVER_PORT); //to jest chyb
+
+            try {
+                Log.i("Debug", "inside try catch");
+                //sends the message to the server
+                mBufferOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                sendMessage(packet.toString());//wysyla pakiet z danymi
+
+                Log.e("RESPONSE FROM SERVER", "S: Received Message: '" + mServerMessage + "'");
+
+            } catch (Exception e) {
+
+                Log.e("TCP", "S: Error", e);
+
+            } finally {
+                //the socket must be closed. It is not possible to reconnect to this socket
+                // after it is closed, which means a new socket instance has to be created.
+                socket.close();
+            }
+
+        } catch (Exception e) {
+
+            Log.e("TCP", "C: Error", e);
+
+        }
+
+    }
+
+
+}
